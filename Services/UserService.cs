77832f40bf808f60eb714a7fdfc4a48c01b90dde
matamike <matamike1990@gmail.com>
2024-11-07 19:50:52 +0200

    Implemented IsActive check on Search User func.

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -7,10 +7,11 @@ public class UserService{
     private readonly TechnicoDbContext _dbContext;
     public UserService(TechnicoDbContext repairApplicationDbContext) => _dbContext = repairApplicationDbContext;
 
-    public bool DeleteOwnerHard(User? user){
-        if (user is null) return false;
+    public bool DeleteOwnerHard(string? vat)
+    {
+        if (vat is null || vat == string.Empty) return false;
 
-        User? ownerQueryResult = _dbContext.Users.FirstOrDefault(c => c.VATNum == user.VATNum); //todo async
+        User? ownerQueryResult = _dbContext.Users.FirstOrDefault(c => c.VATNum == vat); //todo async
         if (ownerQueryResult == null) return false;
 
         //Delete user from the db
@@ -19,10 +20,10 @@ public class UserService{
         return true;
     }
 
-    public bool DeleteOwnerSoft(User? user){
-        if (user is null) return false;
+    public bool DeleteOwnerSoft(string? vat){
+        if (vat is null || vat == string.Empty) return false;
 
-        User? ownerQueryResult = _dbContext.Users.FirstOrDefault(c => c.VATNum == user.VATNum); //todo async
+        User? ownerQueryResult = _dbContext.Users.FirstOrDefault(c => c.VATNum == vat); //todo async
         if (ownerQueryResult == null) return false;
 
         //Deactivate user from the db
@@ -34,6 +35,7 @@ public class UserService{
     public User? SearchUser(string? vat, string? email){
         if (vat == null && email == null) return null;
         User? userQuery = _dbContext.Users.Where(c => c.VATNum == vat || c.Email == email).FirstOrDefault(); //todo async
-        return userQuery; //todo async
+        if(userQuery != null && userQuery.IsActive == true) return userQuery; //todo async
+        else return null;
     }
 }
