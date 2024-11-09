e91d7a36082cbfc30f11834e56a1d723197675a1
p t <panostsoukal@gmail.com>
2024-11-10 01:11:47 +0200

    regex implementation of the user validator

--- TechnicoBackEnd/Validators/UserValidation.cs
+++ TechnicoBackEnd/Validators/UserValidation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TechnicoBackEnd.DTOs;
 using TechnicoBackEnd.Responses;
@@ -9,23 +10,35 @@ using TechnicoBackEnd.Responses;
 namespace TechnicoBackEnd.Validators;
 public class UserValidation : IUserValidation
 {
-    public ResponseApi<UserDTO>? UserValidator(UserDTO repair)
+    public ResponseApi<UserDTO>? UserValidator(UserDTO user)
     {
-        //Check Base properties
+        if (string.IsNullOrWhiteSpace(user.VAT)) 
+            return new ResponseApi<UserDTO> { Status = 1, Description = $"The user's vat must not be null, empty or whitespaces" };
+        if (string.IsNullOrWhiteSpace(user.Name) && Regex.IsMatch(user.VAT, @"^[A-Za-z]+$")) 
+            return new ResponseApi<UserDTO> { Status = 1, Description = $"The user's name must not be null, empty or whitespaces and must only contain alphabet characters" };
+        if (string.IsNullOrWhiteSpace(user.Surname) && Regex.IsMatch(user.VAT, @"^[A-Za-z]+$")) 
+            return new ResponseApi<UserDTO> { Status = 1, Description = $"The user's surname must not be null, empty or whitespaces and must only contain alphabet characters" };
+        if (string.IsNullOrWhiteSpace(user.Address)) 
+            return new ResponseApi<UserDTO> { Status = 1, Description = $"The user's address must not be null, empty or whitespaces" };
+        if (string.IsNullOrWhiteSpace(user.Phone) && Regex.IsMatch(user.VAT, @"^\d+$")) 
+            return new ResponseApi<UserDTO> { Status = 1, Description = $"The user's phone must not be null, empty or whitespaces and must only contain numbers" };
+        if (string.IsNullOrWhiteSpace(user.Email) && Regex.IsMatch(user.VAT, @"^[^@\s]+@[^@\s]+\.[^@\s]+$")) 
+            return new ResponseApi<UserDTO> { Status = 1, Description = $"The user's email must not be null, empty or whitespaces and it must be a valid email address" };
         return null; //todo implement
     }
 
-    public ResponseApi<UserDTO>? UserValidator(UserWithPropertiesDTO repair)
+    public ResponseApi<UserDTO>? UserValidator(UserWithPropertiesDTO user)
     {
-        ResponseApi<UserDTO>? result = UserValidator(repair);
+        ResponseApi<UserDTO>? result = UserValidator(user);
         //Check Additional properties
         return null; //todo implement
     }
 
-    public ResponseApi<UserDTO>? UserValidator(UserWithRequiredFieldsDTO repair)
+    public ResponseApi<UserDTO>? UserValidator(UserWithRequiredFieldsDTO user)
     {
-        ResponseApi<UserDTO>? result = UserValidator(repair);
-        //Check Additional properties
+        ResponseApi<UserDTO>? result = UserValidator(user);
+        if (result == null && string.IsNullOrWhiteSpace(user.Password)) 
+            return new ResponseApi<UserDTO> { Status = 1, Description = $"The user's password must not be null, empty or whitespaces" };
         return null; //todo implement
     }
 
