79f48480cfd3fc9994695bb2c777185a3ca4c235
matamike <matamike1990@gmail.com>
2024-11-09 19:59:52 +0200

    Implemented Validator for User (template - todo implent)

---
+++ TechnicoBackEnd/Validators/UserValidation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicoBackEnd.DTOs;
+using TechnicoBackEnd.Responses;
+
+namespace TechnicoBackEnd.Validators;
+public class UserValidation : IUserValidation
+{
+    public ResponseApi<UserDTO>? UserValidator(UserDTO repair)
+    {
+        //Check Base properties
+        return null; //todo implement
+    }
+
+    public ResponseApi<UserDTO>? UserValidator(UserWithPropertiesDTO repair)
+    {
+        ResponseApi<UserDTO>? result = UserValidator(repair);
+        //Check Additional properties
+        return null; //todo implement
+    }
+
+    public ResponseApi<UserDTO>? UserValidator(UserWithRequiredFieldsDTO repair)
+    {
+        ResponseApi<UserDTO>? result = UserValidator(repair);
+        //Check Additional properties
+        return null; //todo implement
+    }
+
+}
