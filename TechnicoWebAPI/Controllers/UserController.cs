5435b019e382b381fb390f5f043b99ff6677e322
matamike <matamike1990@gmail.com>
2024-11-14 20:11:38 +0200

    Testing changes in pages.

--- TechnicoWebAPI/Controllers/UserController.cs
+++ TechnicoWebAPI/Controllers/UserController.cs
@@ -8,86 +8,50 @@ using TechnicoBackEnd.Models;
 using TechnicoBackEnd.Responses;
 using TechnicoBackEnd.Services;
 
-namespace TechnicoWebAPI.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
-    {
-        private readonly IUserService _userService;
+namespace TechnicoWebAPI.Controllers;
 
-        public UserController(IUserService userService) => _userService = userService;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase{
+    private readonly IUserService _userService;
 
+    public UserController(IUserService userService) => _userService = userService;
 
+    [HttpPost("login")]
+    public async Task<ResponseApi<UserDTO>> Login([FromBody] LoginRequest loginRequest){
+        var user = await _userService.Authenticate(loginRequest.Email, loginRequest.Password);
+        return user;
+    }
 
+    [HttpPost("logout")]
+    public ResponseApi<UserDTO> Logout(){
+        LoginState.UserId = -1;
+        LoginState.IsLoggedIn = false;
+        LoginState.IsAdmin = false;
 
+        return new ResponseApi<UserDTO>() { Status = 1 , Description = "Log out!"};
+    }
 
-        [HttpPost("login")]
-        public IActionResult Login([FromBody] LoginRequest loginRequest)
-        {
-            var user = _userService.Authenticate(loginRequest.Email, loginRequest.Password);
-            if (user != null)
-            {
-                //LoginState.UserId = user.Id;
-                //LoginState.IsLoggedIn = true;
-                //LoginState.IsAdmin = user.Type == UserType.Admin; // Check if the user is an admin
-                return Ok(new { Message = "Login successful", Value = user });
-            }
+    [HttpGet("users")]
+    public async Task<ResponseApi<List<UserDTO>>> GetUsers() => await _userService.GetAllUsers();
 
-            return Unauthorized(new { Message = "Invalid credentials" });
-        }
+    [HttpGet("users/{id}")]
+    public async Task<ResponseApi<UserDTO>> GetCustomer([FromRoute] int id) => await _userService.GetUserDetailsById(id);
 
-        
+    [HttpPost("register_user")]
+    public async Task<ResponseApi<UserDTO>> RegisterUser([FromBody] UserWithRequiredFieldsDTO user) => await _userService.Register(user);
 
+    [HttpPut("update_user")]
+    public async Task<ResponseApi<UserDTO>> UpdateUser([FromBody] UserWithRequiredFieldsDTO user) => await _userService.Update(user);
 
+    [HttpDelete("delete_user_soft/{vat}")]
+    public async Task<ResponseApi<UserDTO>> DeleteUserSoft([FromRoute] string? vat) => await _userService.DeleteSoft(vat);
 
+    [HttpDelete("delete_user_hard/{vat}")]
+    public async Task<ResponseApi<UserDTO>> DeleteUserHard([FromRoute] string? vat) => await _userService.DeleteHard(vat);
 
-
-
-
-
-        [HttpPost("logout")]
-        public IActionResult Logout()
-        {
-            LoginState.UserId = -1;
-            LoginState.IsLoggedIn = false;
-            LoginState.IsAdmin = false;
-            return Ok(new { Message = "Logout successful" });
-        }
-
-
-
-
-
-
-        [HttpGet("users")]
-        public async Task<ResponseApi<List<UserDTO>>> GetUsers() => await _userService.GetAllUsers();
-
-
-        [HttpGet("users/{id}")]
-        public async Task<ResponseApi<UserDTO>> GetCustomer([FromRoute] int id) => await _userService.GetUserDetailsById(id);
-
-        [HttpPost("register_user")]
-        public async Task<ResponseApi<UserDTO>> RegisterUser([FromBody] UserWithRequiredFieldsDTO user) => await _userService.Register(user);
-
-        [HttpPut("update_user")]
-        public async Task<ResponseApi<UserDTO>> UpdateUser([FromBody] UserWithRequiredFieldsDTO user) => await _userService.Update(user);
-
-        [HttpPut("login_user")] //maybe this flag needs to change ??!?!
-        public ResponseApi<UserDTO> LoginUser([FromBody] UserDTO user)//todo change to async
-        {
-            return new ResponseApi<UserDTO> { Status = 1, Description = "LoginUser Not Yet Implemented" };
-        }
-
-        [HttpDelete("delete_user_soft/{vat}")]
-        public async Task<ResponseApi<UserDTO>> DeleteUserSoft([FromRoute] string? vat) => await _userService.DeleteSoft(vat);
-
-        [HttpDelete("delete_user_hard/{vat}")]
-        public async Task<ResponseApi<UserDTO>> DeleteUserHard([FromRoute] string? vat) => await _userService.DeleteHard(vat);
-
-        [HttpGet("search_user")]
-        public async Task<ResponseApi<UserDTO>> SearchUser(string? vat, string? email){
-            return await _userService.Search(vat, email);
-        }
+    [HttpGet("search_user")]
+    public async Task<ResponseApi<UserDTO>> SearchUser(string? vat, string? email){
+        return await _userService.Search(vat, email);
     }
 }
