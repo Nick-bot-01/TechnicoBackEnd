3d4278e36421cef8925831678cddd1ac3d7a7263
matamike <matamike1990@gmail.com>
2024-11-14 12:49:26 +0200

    Added Create Operation (WEB Api Consume) in HomeController

--- TechnicoMVC/Controllers/HomeController.cs
+++ TechnicoMVC/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TechnicoMVC.Controllers;
 
 public class HomeController : Controller{
     private readonly ILogger<HomeController> _logger;
-    private readonly string sourcePrefix = "https://localhost:7017/api/User/";
+    private readonly string sourcePrefix = "https://localhost:7017/api/User/"; //for other controller change to Repair / Property etc.
     private HttpClient client = new HttpClient();
 
     public HomeController(ILogger<HomeController> logger)=> _logger = logger;
@@ -19,23 +19,34 @@ public class HomeController : Controller{
     [HttpGet]
     public async Task<ResponseApi<UserDTO>?> ReadUserToRedirectController(int id)
     {
-        HttpClient client = new HttpClient();
         string url = $"{sourcePrefix}users/{id}";
         var response = await client.GetAsync(url);
         response.EnsureSuccessStatusCode();
         var responseBody = await response.Content.ReadAsStringAsync();
-        Console.WriteLine(responseBody.ToString());
         ResponseApi<UserDTO>? targetUser = System.Text.Json.JsonSerializer.Deserialize<ResponseApi<UserDTO>>(responseBody);
-        Console.WriteLine($"Status: {targetUser?.Status} Description: {targetUser?.Description}");
+        return targetUser;
+    }
 
+    [HttpPost]
+    public async Task<ResponseApi<UserDTO>?> CreateUserToRedirectController(UserWithRequiredFieldsDTO user){
+        string url = $"{sourcePrefix}register_user";
+        var response = await client.PostAsJsonAsync(url, user);
+        var responseBody = await response.Content.ReadAsStringAsync();
+        ResponseApi<UserDTO>? targetUser = System.Text.Json.JsonSerializer.Deserialize<ResponseApi<UserDTO>>(responseBody);
         return targetUser;
     }
 
 
-    public async Task<IActionResult> Index() {
-        int rnd = Random.Shared.Next(-10, 10);
-        var result = await ReadUserToRedirectController(rnd);
-        return View(result);
+    public IActionResult Index() {
+        //int rnd = Random.Shared.Next(-10, 10);
+        //var result = await ReadUserToRedirectController(rnd);
+        return View();
+    }
+
+    //Callback from Add User
+    public async Task<IActionResult> CreateUserCallback(UserWithRequiredFieldsDTO pendingCreationUser){
+        ResponseApi<UserDTO>? createdUSer = await CreateUserToRedirectController(pendingCreationUser);
+        return RedirectToAction("Index");
     }
 
     public IActionResult Privacy() => View();
