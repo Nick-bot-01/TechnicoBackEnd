c299a0d8888edfb5ec4b6ff05241cdcd8dc24f03
matamike <matamike1990@gmail.com>
2024-11-16 11:44:03 +0200

    supplemented usercontroller template (initial stage)

--- TechnicoMVC/Controllers/UserController.cs
+++ TechnicoMVC/Controllers/UserController.cs
@@ -4,11 +4,25 @@ using TechnicoBackEnd.Auth;
 namespace TechnicoMVC.Controllers;
 
 public class UserController : Controller{
+    private readonly ILogger<UserController> _logger;
+    private readonly string sourcePrefix = "https://localhost:7017/api/"; //for other controller change to Repair / Property etc.
+    private HttpClient client = new HttpClient();
+    public UserController(ILogger<UserController> logger) => _logger = logger;
 
+    //Web Api Callbacks
+
+
+
+
+    //Views
     public ActionResult UserHome(){
         if (!LoginState.IsLoggedIn) return RedirectToAction("LandingPage");
 
         if (!LoginState.IsAdmin) return View();
         else return RedirectToAction("LandingPage");
     }
+
+
+
+    //View Callbacks
 }
