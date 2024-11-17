70c22faf37804965dd50dcb4755bf4494c46e937
p t <panostsoukal@gmail.com>
2024-11-17 13:15:49 +0200

    added new dto for user search

---
+++ TechnicoBackEnd/DTOs/QueryUserDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicoBackEnd.DTOs;
+
+public class QueryUserDTO
+{
+    public string? VAT { get; set; }
+    public string? Email { get; set; }
+}
