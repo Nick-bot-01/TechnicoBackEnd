0a0a6457ece84342b3d0bde9a7b64bc8c2c1658f
Nick-bot-01 <75333972+Nick-bot-01@users.noreply.github.com>
2024-11-18 21:44:15 +0200

    Remove old data

--- TechnicoBackEnd/Repositories/TechnicoDbContext.cs
+++ TechnicoBackEnd/Repositories/TechnicoDbContext.cs
@@ -19,35 +19,4 @@ public class TechnicoDbContext : DbContext
         string connectionString = "Data Source=(local);Initial Catalog=Technico; Integrated Security = True;TrustServerCertificate=True;";
         optionsBuilder.UseSqlServer(connectionString);
     }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder){
-
-        modelBuilder.Entity<User>().HasData(
-                    new User()
-                    {
-                        Id = -10,
-                        Name = "Mike",
-                        Surname = "Tyson",
-                        Email = "m.tyson@aol.com",
-                        Phone = "3423423423",
-                        Address = "p0k3b4ll 43",
-                        Password = "Str0ngP4ssw0rd",
-                        VATNum = "M1K3_TYS0N",
-                    },
-                    new User()
-                    {
-                        Id = -9,
-                        Name = "Mikri",
-                        Surname = "Gorgona",
-                        Email = "ariel_onlyfans@gmail.com",
-                        Phone = "34545343423",
-                        Address = "zei sti thalassa se anana 85",
-                        Password = "NotFound",
-                        VATNum = "Sp0ng3B0b",
-                    }
-
-            );
-
-        base.OnModelCreating(modelBuilder);
-    }
 }
